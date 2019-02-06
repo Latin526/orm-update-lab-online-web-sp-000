8c4c0218ade276357f18e9b3f53890b8a16a2ae5
Yasmin Torres <yasmintorres90@hotmail.com>
2019-02-06 13:50:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,7 +37,7 @@ def save
     self.update
   else
     sql = <<-SQL
-      INSERT INTO student (name, grade)
+      INSERT INTO student (name, grade, id)
       VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
